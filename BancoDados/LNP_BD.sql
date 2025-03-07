7cd9540b13d1815e90c514847a5b3b06674e14cb
Patrick Gonzales <patrickgonzales031@gmail.com>
2025-03-07 20:00:21 -0300

    bd

--- BancoDados/LNP_BD.sql
+++ BancoDados/LNP_BD.sql
@@ -12,10 +12,27 @@ senha text not null,
 stats boolean
 );
 
-insert into register (id,username,cpf,email,grupo,senha,stats) values (null, 'Patrick', '4126166980', 'patrickgonzales031@gmail.com', 'Adm', '123', true),
-(null, 'Nicolas', '54002619819', 'nvs.silva@gmail.com', 'Adm', '123', true),
-(null, 'Lucas', '46372001802', 'contadolucas40@gmail.com', 'Adm', '123', true);
+alter table produto
+add column stats boolean not null;
 
-select * from register;
+create table produto(
+id integer auto_increment primary key,
+cod integer not null unique,
+nome varchar(200) not null,
+qtd integer not null,
+valor decimal(10,2) not null,
+avaliacao decimal (2,1),
+descricao varchar(2000)
+);
 
-SELECT VERSION();
+select * from imgProd;
+
+create table imgProd(
+idImg integer auto_increment primary key,
+nome varchar(200) not null,
+linkimg varchar(250) not null,
+padrao boolean,
+produtoId integer not null,
+foreign key (produtoId) references produto(id)
+);
+

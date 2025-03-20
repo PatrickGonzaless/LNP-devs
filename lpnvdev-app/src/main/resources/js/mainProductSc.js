09eca12fb9aff48d1fd933e543ecf809c5d045ca
Patrick Gonzales <patrickgonzales031@gmail.com>
2025-03-20 11:12:01 -0300

    Tela principal de produtos

---
+++ lpnvdev-app/src/main/resources/js/mainProductSc.js
@@ -0,0 +1,77 @@
+const loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
+
+function verifUser() {
+    if (
+        !(loggedInUser.grupo === "Administrador") &&
+        !(loggedInUser.grupo === "Adm") &&
+        !(loggedInUser.grupo === "Estoquista") &&
+        !(loggedInUser.grupo === "none")
+    ) {
+        alert("Você não tem permissão para visualizar esta página.");
+        window.location.href = "/pagina-nao-autorizada.html";
+        return;
+    }
+
+    listProduct();
+}
+
+function listProduct() {
+    fetch(`http://localhost:8080/product`, {
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        method: "GET",
+    })
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(
+                    `Erro na requisição: ${res.status} - ${res.statusText}`
+                );
+            }
+            return res.json();
+        })
+        .then((data) => {
+            listarProdutos(data);
+        })
+        .catch((err) => {
+            alert("Erro ao buscar produtos: " + err.message);
+        });
+}
+
+function listarProdutos(produtos) {
+    let cardContainer = document.getElementById("cardContainer");
+    if (!cardContainer) {
+        console.error("Elemento cardContainer não encontrado!");
+        return;
+    }
+
+    cardContainer.innerHTML = "";
+
+    produtos.forEach((produto) => {
+        let card = `
+        <div class="cardExCont">
+            <div class="cardEx">
+                <div class="img_space">
+                    <div class="rating">
+                        <span>${produto.avaliacao ? produto.avaliacao.toFixed(1) : "N/A"}</span> /5.0
+                    </div>
+                </div>
+                <div class="productName"><p>${produto.nome}</p></div>
+                <div class="productPrice">
+                    <p>Valor do produto: R$ <span class="price">${produto.valor ? produto.valor.toFixed(2) : "0.00"}</span></p>
+                </div>
+                <div class="btnCont">
+                    <button>Detalhes</button>
+                </div>
+            </div>
+        </div>
+        `;
+
+        cardContainer.insertAdjacentHTML("beforeend", card);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    verifUser();
+});
